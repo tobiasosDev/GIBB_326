5d61113392cdec8541c450bdb638321a731cec35
Tobias Lüscher <tobias@lscher.com>
2016-10-24 17:23:02 +0200

    Test tests

---
+++ src/test/MainTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by tobiasluscher on 24.10.16.
+ */
+public class MainTest extends TestCase {
+    @org.junit.Test
+    public void start() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void main() throws Exception {
+
+    }
+r
+    public void testStart() throws Exception {
+
+    }
+
+}
