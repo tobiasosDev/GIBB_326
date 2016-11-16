e1bffc2ce61acd1684a3c6d93f4d731100e9b132
lukas nydegger <nydegger.lukas@gmail.com>
2016-11-16 13:25:34 +0100

    Singelton

--- bomberman/src/main/java/Model/Field.java
+++ bomberman/src/main/java/Model/Field.java
@@ -4,10 +4,24 @@ package Model;
  * Created by lukas on 08.11.2016.
  */
 public class Field {
+
+    private static Field instance = null;
+
+    protected Field() {
+        // Exists only to defeat instantiation.
+    }
+
     private GameElement[] gameElements;
     private int size;
 
     public void draw(){
 
+    }
+
+    public static Field getInstance() {
+        if(instance == null) {
+            instance = new Field();
+        }
+        return instance;
     }
 }
